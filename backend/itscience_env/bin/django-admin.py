564f5d40299b8de7ffdf4064f1a98ca8d2f1c1be
User <user@Users-MacBook-Pro.local>
2019-12-01 12:37:30 +0200

    django project created

---
+++ backend/itscience_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/user/Documents/Study/Software engineering/IT-Science/backend/itscience_env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
