049706ebc0ac4fe8425242105fbd2a07b425f897
User <user@Users-MacBook-Pro.local>
2019-12-01 18:22:10 +0200

    created models in materials_system

--- backend/itScience/materials_system/models.py
+++ backend/itScience/materials_system/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+    #user = Models.OneToOneField()
+
+class Post(models.Model):
+    moderator = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    short_title = models.CharField(max_length=150)
+    full_title = models.CharField(max_length=300)
+    description = models.CharField(max_length=500)
+    published = models.DateTimeField(auto_now=True)
+    title_image = models.ImageField()
+    
+
+class HashTag(models.Model):
+    pass
