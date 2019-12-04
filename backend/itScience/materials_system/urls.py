597018106d5ee2521e322dc2ef2fda77095f2374
User <user@Users-MacBook-Pro.local>
2019-12-05 00:17:42 +0200

    about added to urls

--- backend/itScience/materials_system/urls.py
+++ backend/itScience/materials_system/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', TemplateView.as_view(template_name='about.html'), name='about'),
     path('posts/', views.PostsView.as_view(), name='posts'),
     path('it/', views.ItPostView.as_view(), name='it'),
     path('programming/', views.ProgrammingPostView.as_view(), name='programming'),
