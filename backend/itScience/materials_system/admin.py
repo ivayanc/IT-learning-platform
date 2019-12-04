8264229db2fb1068c26cdba987185a3c3fc7803f
User <user@Users-MacBook-Pro.local>
2019-12-04 23:13:28 +0200

    small fix

--- backend/itScience/materials_system/admin.py
+++ backend/itScience/materials_system/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, ItPost, ProgrammingPost, SchoolPost
 
 admin.site.register(Post)
+admin.site.register(SchoolPost)
+admin.site.register(ItPost)
+admin.site.register(ProgrammingPost)
