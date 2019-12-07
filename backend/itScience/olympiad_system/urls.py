51fade0807a69dd5ecb4e9dde34b61615bf9e4f2
User <user@Users-MacBook-Pro.local>
2019-12-07 16:30:13 +0200

    verification page template added

--- backend/itScience/olympiad_system/urls.py
+++ backend/itScience/olympiad_system/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='olympiad_system/olympiad_sytem.html'), name='olympiad_system'),
     path('<int:pk>/', TemplateView.as_view(template_name='olympiad_system/olympiad.html'), name='olympiad'),
     #path('<int:pk>/<int:pk>', views.ItPostView.as_view(), name='task'),
-    #path('<int:pk>/<int:pk>/verify', views.ProgrammingPostView.as_view(), name='task_verify'),
+    path('verify/', TemplateView.as_view(template_name='olympiad_system/verify.html'), name='task_verify'),
     #path('school/', views.SchoolPostView.as_view(), name='school'),
     #path('scratch/', views.ScratchPostView.as_view(), name='scratch'),
     #path('posts/', views.PostView.as_view(), name='posts'),
