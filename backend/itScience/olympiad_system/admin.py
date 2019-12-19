913186846724617d3c6d3b569ea162a851d6406a
damhuman <anton.dasyuk@gmail.com>
2019-12-19 16:55:20 +0200

    working commit

--- backend/itScience/olympiad_system/admin.py
+++ backend/itScience/olympiad_system/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    
+class CriteriaInline(admin.TabularInline):
+    model = Criteria
+    
+
+@admin.register(Olympiad)
+class OlympiadAdmin(admin.ModelAdmin):
+    list_display = ('title', 'olymp_type', 'start_time', 'duration')
+    model = Olympiad
+    inlines = (TaskInline,)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task_type', 'olympiad')
+    model = Task
+    inlines = (CriteriaInline,)
--- backend/itScience/olympiad_system/models.py
+++ backend/itScience/olympiad_system/models.py
@@ -13,15 +13,40 @@ class Olympiad(models.Model):
     title           = models.CharField(verbose_name="Назва олімпіади", max_length=256)
     duration        = models.DurationField(verbose_name="Час проведення")
     description     = models.TextField(verbose_name="Опис олімпіади")
-    olymp_type      = models.PositiveSmallIntegerField(
+    olymp_type      = models.PositiveSmallIntegerField(verbose_name="Тип проведення",
                             choices = TYPE_CHOICES,
                             default = PUBLIC,
     )
     
+    def __str__(self):
+        return self.title
+    
 class Task(models.Model):
+    EXCEL = 1
+    ACCESS = 2
+    WORD = 3
+    POWERPOINT = 4
+    OTHER = 5
+    TYPE_CHOICES    = [
+        (EXCEL, ('Excel')),
+        (ACCESS, ('Access')),
+        (WORD, ('Word')),
+        (POWERPOINT, ('PowerPoint')),
+        (OTHER, ('Інше')),
+    ]
+
     olympiad        = models.ForeignKey(Olympiad,on_delete=models.CASCADE, verbose_name="Олімпіада")
+    title           = models.CharField(verbose_name="Назва задачі", max_length=256)
     description     = models.TextField(verbose_name="Опис задачі")
     files           = models.FileField(verbose_name="Додаткові матеріали", blank=True)
+    task_type       = models.PositiveSmallIntegerField(verbose_name="Розділ задачі",
+                            choices = TYPE_CHOICES,
+                            default = OTHER,
+    )
+
+    def __str__(self):
+        return self.title
+
 
 class Solution(models.Model):
     user            = models.ForeignKey(SystemUser, on_delete=models.CASCADE, verbose_name="Відправник", related_name='sender')
@@ -30,12 +55,7 @@ class Solution(models.Model):
     reviewer        = models.ForeignKey(SystemUser, on_delete=models.CASCADE, blank=True, related_name='reviewer')
 
 class Criteria(models.Model):
+    task            = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name="Задача")
     descrition      = models.CharField(verbose_name="Опис критерія", max_length=256)
     max_value       = models.IntegerField(verbose_name="Максимальний бал")
     step            = models.FloatField(verbose_name="Крок оцінки")
-
-class TaskCriterias(models.Model):
-    task            = models.ForeignKey(Task, on_delete=models.CASCADE, verbose_name="Задача")
-    criteria        = models.ForeignKey(Criteria, on_delete=models.CASCADE, verbose_name="Критерій")
-
-
