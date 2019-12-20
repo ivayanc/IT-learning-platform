3921d86590b720d6ef33cb6408106eda8e439b2c
damhuman <anton.dasyuk@gmail.com>
2019-12-20 04:41:18 +0200

    single olymp page

--- backend/itScience/olympiad_system/urls.py
+++ backend/itScience/olympiad_system/urls.py
@@ -5,9 +5,9 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='olympiad_system/olympiad_sytem.html'), name='olympiad_system'),
     path('', views.IndexView.as_view(), name='olympiad_system'),
-    path('<int:pk>/', TemplateView.as_view(template_name='olympiad_system/olympiad.html'), name='olympiad'),
+    #path('<int:id>/', TemplateView.as_view(template_name='olympiad_system/olympiad.html'), name='olympiad'),
+    path('<int:id>/', views.OlympiadView.as_view(), name='olympiad'),
     path('<int:pk>/<int:id>', TemplateView.as_view(template_name='olympiad_system/task.html'), name='task'),
     path('verify/', TemplateView.as_view(template_name='olympiad_system/verify.html'), name='task_verify'),
     path('<int:pk>/verify/', TemplateView.as_view(template_name='olympiad_system/verify_list.html'), name='verify_list'),
--- backend/itScience/olympiad_system/views.py
+++ backend/itScience/olympiad_system/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from materials_system.models import Post
 from .models import Olympiad
@@ -18,12 +18,26 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        context['registred_olympiads'] = Olympiad.objects.all().filter(participants__in=[self.request.user], is_ended=False).order_by('start_time')[:6]
-        context['upcoming_olympiads'] = Olympiad.objects.filter(olymp_type=Olympiad.PUBLIC).exclude(participants__in=[self.request.user]).order_by('start_time')[:6]
-        context['ended_olympiads'] = Olympiad.objects.filter(participants__in=[self.request.user], is_ended=True).order_by('start_time')[:6]
+
+        context['upcoming_olympiads'] = Olympiad.objects.filter(olymp_type=Olympiad.PUBLIC).order_by('start_time')[:6]
+        if self.request.user.is_authenticated:
+            context['upcoming_olympiads'] = context['upcoming_olympiads'].exclude(participants__in=[self.request.user]).order_by('start_time')[:6]
+            context['registred_olympiads'] = Olympiad.objects.all().filter(participants__in=[self.request.user], is_ended=False).order_by('start_time')[:6]
+            context['ended_olympiads'] = Olympiad.objects.filter(participants__in=[self.request.user], is_ended=True).order_by('start_time')[:6]
         return context
 
+class OlympiadView(DetailView):
+    template_name = 'olympiad_system/olympiad.html'
+    
+    def get_object(self, queryset=None):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Olympiad, pk=id_)
+
+    def get_context_data(self, **kwargs):
+        context = super(OlympiadView, self).get_context_data(**kwargs) # get the default context data
+        #context['tasks'] = Post.objects.filter(favorite__in=[self.get_object().pk])
+        print(context)
+        return context
 
 # class VerifySolutionView(DetailView):
 #     template_name = 'olympiad_system/verify.html'
